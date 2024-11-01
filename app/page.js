a439ea2ac4f98dafbf08227bfa87806f6c92e72d
Jose Luis DLS <jdelossantos1@ucol.mx>
2024-11-01 01:26:55 -0600

    Changed name Losanto Parfums CRUD

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="w-screen py-20 flex justify-center flex-col items-center">
       <div className="flex items-center justify-between gap-1 mb-5">
-        <h1 className="text-4xl font-bold">Next.js 14 Node Express CRUD | MySQL </h1>
+        <h1 className="text-4xl font-bold">Losanto Parfums | CRUD</h1>
       </div>
       <div className="overflow-x-auto">
         <div className="mb-2 w-full text-right">
